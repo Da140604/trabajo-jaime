2647132d12c0bd40cfa8e121b434b28173d42493
Da140604 <dseglop1710@g.educaand.es>
2025-02-10 12:13:00 +0100

    Create pedirnumerico.js

---
+++ pedirnumerico.js
@@ -0,0 +1,9 @@
+function pedirNumeroEnRango(min, max) {
+  var numero;
+  do {
+    numero = prompt("Ingresa un número entre " + min +" y " + max + ":");
+    numero = Number(numero);
+  }  While ( numero < min || numero > max);
+
+return numero;
+}
