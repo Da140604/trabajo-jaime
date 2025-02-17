d38ab2e62a94f09a995362060f1fc528830ed087
Da140604 <dseglop1710@g.educaand.es>
2025-02-17 12:15:57 +0100

    Create compromarsiesprimo.js

---
+++ compromarsiesprimo.js
@@ -0,0 +1,15 @@
+function comprobarprimo (numero)
+  {
+var esprimo;
+    var i;
+for(i=numero-1; i>=2; i=i-1)
+  {
+    if ( numero % i == 0 )
+    {
+      esprimo = false;
+    }
+    }
+esprimo = true;
+    
+return esprimo;
+    }
